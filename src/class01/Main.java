453bf5a5390474349576ebd64dd4479e6eaa9e63
Massoud-Babak <w1689000@apps.losrios.com>
2023-02-23 21:54:43 -0800

    thirdUpdate

--- src/class01/Main.java
+++ src/class01/Main.java
@@ -6,26 +6,50 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-      System.setProperty("webdriver.chrome.driver","Driver/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        // to open the Website twitter.com
-        driver.get("https://facebook.com");
+      //tell your project where the webDriver is located
 
-        // get the current URL That is there in the browser
+      System.setProperty("webdriver.chrome.driver", "Driver/chromedriver");
 
-        String URL = driver.getCurrentUrl();
+      // create an instance of webDriver
+      WebDriver driver = new ChromeDriver();
 
-        // Print out the url
-        System.out.println(URL);
+      // to open the google.com
+      driver.get("https://www.google.com");
 
-        // to get the title of the webpage
-        String title = driver.getTitle();
-        System.out.println("title of the page is "+ title);
+      // then from google it will navigate to facebook
+      driver.navigate().to("https://facebook.com");
 
-        // to hold the browser for 3000 milSecond = 3 second.
-        Thread.sleep(3000);
+      // hold the page for 3 seconds
+      Thread.sleep(3000);
 
-        // to close the browser
-        driver.quit();
+      // will navigate back to google from facebook page
+      driver.navigate().back();
+
+      // will hold in google for 3 seconds again
+      Thread.sleep(3000);
+
+      // after 3 second hold in google page, will navigatge back to facebook page
+      driver.navigate().forward();
+
+      // to refresh the page
+
+      driver.navigate().refresh();
+
+      // get the current URL That is there in the browser
+
+      String URL = driver.getCurrentUrl();
+
+      // Print out the url
+      System.out.println(URL);
+
+      // to get the title of the webpage
+      String title = driver.getTitle();
+      System.out.println("title of the page is "+ title);
+
+      // to hold the browser for 3000 milSecond = 3 second.
+      Thread.sleep(3000);
+
+      // to close the browser
+      driver.quit();
     }
 }
